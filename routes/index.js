3c3565e4520d0953d2cfe7207b73f1285be2e49f
zoar <zoar@k-side.net>
2018-03-12 15:00:54 +0900

    Add function get unixtime

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
+
 const sphericalmercator = require('@mapbox/sphericalmercator');
 const merc = new sphericalmercator();
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./mokuroku.sqlite');
+
+const moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,10 +17,29 @@ router.get('/', function(req, res, next) {
 router.get('/:z/:x/:y.json', (req, res, next) => {
    const tileParam = req.params;
    const wsen = merc.bbox(tileParam.x, tileParam.y, tileParam.z);
-   const path = tileParam.z + "/" + tileParam.x + "/" + tileParam.y + ".png";
-   console.log(path);
+   const path = tileParam.z +
+      "/" + tileParam.x +
+      "/" + tileParam.y + ".png";
+   
+   db.serialize(() => {
+      db.get(
+         'SELECT unixtime FROM mokuroku WHERE path = ?',
+         path,
+         (err, result) => {
+         if (err) throw err;
+         const updateTime = moment
+            .unix(result.unixtime)
+            .format('HH:mm:ss');
+         const updateDate = moment
+            .unix(result.unixtime)
+            .format('YYYY-MM-DD');
+
+         
+      });
+   });
    res.json({
-      "x": tileParam.x
+      "x": tileParam.x,
+      "y": tileParam.y
    });
 });
 
