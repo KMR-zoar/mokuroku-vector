9d1d4f8e6dcdadd6c12f9f9f448fa821f61d6886
zoar <zoar@k-side.net>
2018-03-12 14:19:10 +0900

    Get boundary

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const sphericalmercator = require('@mapbox/sphericalmercator');
+const merc = new sphericalmercator();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
    res.render('index', { title: 'Express' });
+});
+
+router.get('/:z/:x/:y.json', (req, res, next) => {
+   const tileParam = req.params;
+   const wsen = merc.bbox(tileParam.x, tileParam.y, tileParam.z);
+   const path = tileParam.z + "/" + tileParam.x + "/" + tileParam.y + ".png";
+   console.log(path);
+   res.json({
+      "x": tileParam.x
+   });
 });
 
 module.exports = router;
