5873993043a0d9e66d05d1e8b689e78114db8c80
zoar <zoar@k-side.net>
2019-04-12 15:14:16 +0900

    タイル画像としての機能を実装

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./mokuroku.sqlite');
 
 const moment = require('moment');
+const canvas = require('canvas');
 
 createGeoJSON = (date, time, unixtime, path, coordinates) => {
   let result = require('../template.json');
@@ -20,6 +21,70 @@ createGeoJSON = (date, time, unixtime, path, coordinates) => {
   return result;
 };
 
+const GeoJSONCreater = (sqlResult, coordinates, path) => {
+  let unixtime;
+  let updateTime;
+  let updateDate;
+
+  if (sqlResult) {
+    unixtime = sqlResult.unixtime;
+    updateTime = moment.unix(unixtime).format('HH:mm:ss');
+    updateDate = moment.unix(unixtime).format('YYYY-MM-DD');
+  } else {
+    unixtime = null;
+    updateTime = '目録が存在しないエリアです';
+    updateDate = '';
+  }
+
+  funcResult = createGeoJSON(
+    updateDate,
+    updateTime,
+    unixtime,
+    path,
+    coordinates
+  );
+  return funcResult;
+};
+
+const PNGCreater = (sqlResult, callback) => {
+  let unixtime;
+  let updateTime;
+  let updateDate;
+  let text;
+
+  if (sqlResult) {
+    unixtime = sqlResult.unixtime;
+    updateTime = moment.unix(unixtime).format('HH:mm:ss');
+    updateDate = moment.unix(unixtime).format('YYYY-MM-DD');
+
+    text =
+      'date: ' +
+      updateDate +
+      '\n' +
+      'time: ' +
+      updateTime +
+      '\n' +
+      'unixtime: ' +
+      unixtime;
+  } else {
+    text = '\n目録が存在しないエリアです';
+  }
+
+  const vCanvas = canvas.createCanvas(256, 256);
+  const ctx = vCanvas.getContext('2d');
+
+  canvas.loadImage('./routes/background.png').then(image => {
+    ctx.drawImage(image, 0, 0, 256, 256);
+    ctx.font = '15px Impact';
+    ctx.fillStyle = 'green';
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+    ctx.lineTo(0, 256);
+    ctx.stroke();
+    ctx.fillText(text, 30, 110);
+    callback(vCanvas.toDataURL('image/png'));
+  });
+};
+
 router.get('/', function(req, res, next) {
   var accessUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
   res.render('index', {
@@ -28,7 +93,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/:z/:x/:y.json', (req, res, next) => {
+router.get('/:z/:x/:y.:format', (req, res, next) => {
   const tileParam = req.params;
   const wsen = merc.bbox(tileParam.x, tileParam.y, tileParam.z);
   const coordinates = [
@@ -49,29 +114,22 @@ router.get('/:z/:x/:y.json', (req, res, next) => {
       (err, result) => {
         if (err) throw err;
 
-        let unixtime;
-        let updateTime;
-        let updateDate;
-
-        if (result) {
-          unixtime = result.unixtime;
-          updateTime = moment.unix(unixtime).format('HH:mm:ss');
-          updateDate = moment.unix(unixtime).format('YYYY-MM-DD');
+        if (req.params.format === 'json') {
+          const funcResult = GeoJSONCreater(result, coordinates, path);
+          res.json(funcResult);
+        } else if (req.params.format === 'png') {
+          PNGCreater(result, image => {
+            const imageBody = image.split(',')[1];
+            const img = Buffer.from(imageBody, 'base64');
+            res.writeHead(200, {
+              'Content-Type': 'image/png',
+              'Content-Length': img.length
+            });
+            res.end(img);
+          });
         } else {
-          unixtime = null;
-          updateTime = '目録が存在しないエリアです';
-          updateDate = '';
+          res.status(404).send('Not Found');
         }
-
-        result = createGeoJSON(
-          updateDate,
-          updateTime,
-          unixtime,
-          path,
-          coordinates
-        );
-
-        res.json(result);
       }
     );
   });
