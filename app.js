2f0a7494624b92c9cd4f9e6763710aa3114e1c7a
zoar <zoar@k-side.net>
2019-04-12 13:46:34 +0900

    インデントを2に変更

--- app.js
+++ app.js
@@ -22,9 +22,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
 });
 
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -8,60 +8,73 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./mokuroku.sqlite');
 
 const moment = require('moment');
-const url = require('url');
 
 createGeoJSON = (date, time, unixtime, path, coordinates) => {
-   let result = require('../template.json');
-   result.features[0].properties.date = date;
-   result.features[0].properties.time = time;
-   result.features[0].properties.unixtime = unixtime;
-   result.features[0].properties.tilePath = path;
-   result.features[0].geometry.coordinates = coordinates;
+  let result = require('../template.json');
+  result.features[0].properties.date = date;
+  result.features[0].properties.time = time;
+  result.features[0].properties.unixtime = unixtime;
+  result.features[0].properties.tilePath = path;
+  result.features[0].geometry.coordinates = coordinates;
 
-   return result;
-}
+  return result;
+};
 
 router.get('/', function(req, res, next) {
-   var accessUrl = req.protocol + "://" +
-      req.get('host') + req.originalUrl;
-   res.render('index', {
-      title: 'Mokuroku-Vector',
-      url: accessUrl
-   });
+  var accessUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  res.render('index', {
+    title: 'Mokuroku-Vector',
+    url: accessUrl
+  });
 });
 
 router.get('/:z/:x/:y.json', (req, res, next) => {
-   const tileParam = req.params;
-   const wsen = merc.bbox(tileParam.x, tileParam.y, tileParam.z);
-   const coordinates = [[
+  const tileParam = req.params;
+  const wsen = merc.bbox(tileParam.x, tileParam.y, tileParam.z);
+  const coordinates = [
+    [
       [wsen[0], wsen[3]],
       [wsen[2], wsen[3]],
       [wsen[2], wsen[1]],
       [wsen[0], wsen[1]],
       [wsen[0], wsen[3]]
-   ]];
-   const path = tileParam.z +
-      "/" + tileParam.x +
-      "/" + tileParam.y + ".png";
-   
-   db.serialize(() => {
-      db.get(
-         'SELECT unixtime FROM mokuroku WHERE path = ?',
-         path,
-         (err, result) => {
-         if (err) throw err;
-         const updateTime = moment
-            .unix(result.unixtime)
-            .format('HH:mm:ss');
-         const updateDate = moment
-            .unix(result.unixtime)
-            .format('YYYY-MM-DD');
+    ]
+  ];
+  const path = tileParam.z + '/' + tileParam.x + '/' + tileParam.y + '.png';
 
-         result = createGeoJSON(updateDate, updateTime, result.unixtime, path, coordinates);
+  db.serialize(() => {
+    db.get(
+      'SELECT unixtime FROM mokuroku WHERE path = ?',
+      path,
+      (err, result) => {
+        if (err) throw err;
 
-         res.json(result);
-      });
-   });
+        let unixtime;
+        let updateTime;
+        let updateDate;
+
+        if (result) {
+          unixtime = result.unixtime;
+          updateTime = moment.unix(unixtime).format('HH:mm:ss');
+          updateDate = moment.unix(unixtime).format('YYYY-MM-DD');
+        } else {
+          unixtime = null;
+          updateTime = '目録が存在しないエリアです';
+          updateDate = '';
+        }
+
+        result = createGeoJSON(
+          updateDate,
+          updateTime,
+          unixtime,
+          path,
+          coordinates
+        );
+
+        res.json(result);
+      }
+    );
+  });
 });
 
 module.exports = router;
